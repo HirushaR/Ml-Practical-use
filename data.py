da9429fabac8edda647419b4b05b227e90b690c1
HirushaR <hirusharandunu11@gmail.com>
2020-03-01 15:11:53 +0530

    forcst

--- data.py
+++ data.py
@@ -1,23 +1,30 @@
+import quandl, math
+import numpy as np
 import pandas as pd
-import quandl
-import math
+from sklearn import preprocessing, svm
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
-#get data from qundl google data
-df = quandl.get('WIKI/GOOGL')
+df = quandl.get("WIKI/GOOGL")
+df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
-df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
-#high low precent
-df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
-df['%_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
-
-df = df[['Adj. Close', 'HL_PCT', '%_change', 'Adj. Volume']]
-
+df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 forecast_col = 'Adj. Close'
-df.fillna(-99999, inplace=True)
-
-forecast_out = int(math.ceil(0.01*len(df)))
-
+df.fillna(value=-99999, inplace=True)
+forecast_out = int(math.ceil(0.01 * len(df)))
 df['label'] = df[forecast_col].shift(-forecast_out)
 
+print(forecast_out)
+X = np.array(df.drop(['label'], 1))
+X = preprocessing.scale(X)
+X = X[:-forecast_out]
 df.dropna(inplace=True)
-print(df.head())
+y = np.array(df['label'])
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+confidence = clf.score(X_test, y_test)
+print(confidence)
