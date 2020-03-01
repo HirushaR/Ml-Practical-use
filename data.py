37386bd7ad4102080cf46992e0d04c5690348f42
HirushaR <hirusharandunu11@gmail.com>
2020-03-01 14:12:48 +0530

    features

--- data.py
+++ data.py
@@ -1,12 +1,23 @@
 import pandas as pd
 import quandl
+import math
 
 #get data from qundl google data
 df = quandl.get('WIKI/GOOGL')
 
 df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+#high low precent
 df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
-df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+df['%_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
 
-df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
+df = df[['Adj. Close', 'HL_PCT', '%_change', 'Adj. Volume']]
+
+forecast_col = 'Adj. Close'
+df.fillna(-99999, inplace=True)
+
+forecast_out = int(math.ceil(0.01*len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+df.dropna(inplace=True)
 print(df.head())
